ab5eb8bed898ed19a7dddc283bb8fe0ead3bbae6
Zach <zdyar89@gmail.com>
2025-06-19 15:54:52 -0400

    .

---
+++ TaxDemo.Tests/ChromeTests.cs
@@ -0,0 +1,52 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxDemo.Tests
+{
+    [TestFixture]
+    public class ChromeTests
+    {
+        private ChromeDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://localhost:32783/calculator");
+        }
+        [Test]
+        public void CheckPageTitle()
+        {
+            var title = driver.FindElement(By.TagName("h1")).ToString();
+            Console.WriteLine(title);
+            Assert.That("Tax Calculator Input" == title);
+        }
+
+        [Test]
+        public void CheckSalaryFieldExists()
+        {
+            var salaryField = driver.FindElement(By.Id(""));
+            Assert.That(salaryField != null);
+        }
+        [Test]
+        public void CheckCalculationButtonExists()
+        {
+            var calculationButton = driver.FindElement(By.Id(""));
+            Assert.That(calculationButton != null);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+
+
+    }
+}
